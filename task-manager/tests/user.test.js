ff8322425a08751fd116f33358a28e333c242282
Amrut Patil <amrut.patil@hotmail.com>
2019-06-29 23:27:28 +0530

    Unit testing completed

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,24 +1,9 @@
 const request = require('supertest');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
 const app = require('../src/app');
 const User = require('../src/models/user');
+const { userOne, userOneId, setupDatabase } = require('./fixtures/db');
 
-const userOneId = new mongoose.Types.ObjectId();
-const userOne = {
-    _id: userOneId,
-    name: "admin",
-    email: "admin@email.com",
-    password: "admin1234",
-    tokens: [{
-        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
-    }]
-}
-
-beforeEach(async () => {
-    await User.deleteMany();
-    await new User(userOne).save();
-})
+beforeEach(setupDatabase);
 
 test('Should signup a new user', async () => {
     const response = await request(app).post('/users').send({
@@ -105,12 +90,12 @@ test('Should upload avatar image', async () => {
 
 test('Should update valid user fields', async () => {
     await request(app)
-    .patch('/users/me')
-    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
-    .send({
-        name: 'amrut'
-    })
-    .expect(200);
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'amrut'
+        })
+        .expect(200);
 
     const user = await User.findById(userOneId);
     expect(user.name).toEqual('amrut');
@@ -118,10 +103,10 @@ test('Should update valid user fields', async () => {
 
 test('Should not update invalid user fields', async () => {
     await request(app)
-    .patch('/users/me')
-    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
-    .send({
-        location: 'pune'
-    })
-    .expect(400);
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'pune'
+        })
+        .expect(400);
 })
